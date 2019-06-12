e62684090af309a4d39e2183690d5eb3c6fe7cf8
Nicholas Stano <nicholasstano@gmail.com>
2019-06-12 09:55:38 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -134,7 +134,7 @@ def adding_to_dennis
         }
      }
 
-programmer_hash[:dennis_ritchie] = {:languages => ["C", "Assembly"]}
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 return programmer_hash
 
 end
